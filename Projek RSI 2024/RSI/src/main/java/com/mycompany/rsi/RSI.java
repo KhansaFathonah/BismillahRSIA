4d3b535fe1b301779fca97fbf70f58e657900f22
Khansa Rafi'ul Fathonah <khansafathonahf@gmail.com>
2024-11-12 17:00:59 +0700

    Add files via upload

---
+++ Projek RSI 2024/RSI/src/main/java/com/mycompany/rsi/RSI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rsi;
+
+/**
+ *
+ * @author Asus
+ */
+public class RSI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
