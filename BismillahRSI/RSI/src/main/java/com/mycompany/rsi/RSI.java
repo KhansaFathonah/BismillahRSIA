03397d1ab85e9599b5bec84aed4346fbd988708c
Asus <Asus@LAPTOP-5QLNQ3Q8>
2024-11-12 19:06:54 +0700

    Update RSI.java

--- BismillahRSI/RSI/src/main/java/com/mycompany/rsi/RSI.java
+++ BismillahRSI/RSI/src/main/java/com/mycompany/rsi/RSI.java
@@ -14,5 +14,6 @@ public class RSI {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
