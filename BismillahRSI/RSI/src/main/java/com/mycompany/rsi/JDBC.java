72b50bbf7eab82f66794300bfb6ceb521dd77007
Asus <Asus@10.200.179.184>
2024-11-15 19:57:47 +0700

    Revert "Update RSI.java"
    This reverts commit 664b3315c4d54365a4961c7da80717a57dd0ec6e.

--- BismillahRSI/RSI/src/main/java/com/mycompany/rsi/JDBC.java
+++ BismillahRSI/RSI/src/main/java/com/mycompany/rsi/JDBC.java
@@ -1,42 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.mycompany.rsi;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
+/**
+ *
+ * @author Asus
+ */
 public class JDBC {
-    // JDBC URL, username, and password of MySQL server
-    private static final String URL = "jdbc:mysql://localhost:3306/parenta";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
-
-    // JDBC variables for opening, closing, and managing connection
-    private static Connection connection;
-
-    public static Connection getConnection() {
-        if (connection == null) {
-            try {
-                // Load and register the MySQL JDBC driver
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                // Establish the connection
-                connection = DriverManager.getConnection(URL, USER, PASSWORD);
-                System.out.println("Database connection established.");
-            } catch (ClassNotFoundException | SQLException e) {
-                e.printStackTrace();
-                System.out.println("Failed to establish database connection.");
-            }
-        }
-        return connection;
-    }
-
-    public static void closeConnection() {
-        if (connection != null) {
-            try {
-                connection.close();
-                System.out.println("Database connection closed.");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+    
 }
